63aec91a212b2b6b2fb6eee5f17bec3563077aef
Anthony <Anthony@Anthonys-iMac.local>
2017-10-15 17:48:07 -0700

    loops

--- basics/Program.cs
+++ basics/Program.cs
@@ -4,9 +4,18 @@ namespace basics
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+            //Interpolation
+            // Console.WriteLine($"The answer to 2 + 7 is {2+7}");
+            //Loops
+            // for (int i = 1; i <= 5; i++) {
+            //     Console.WriteLine(i);
+            // } 
+            // int j = 1;
+            // while (j<6){
+            //     Console.WriteLine(j);
+            //     j = j + 1;
+            // }
         }
     }
 }
