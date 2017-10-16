591bcdf796820265961a0d35db95dc3dff97d910
Hoopes31 <anthonyhoopes@gmail.com>
2017-10-15 18:48:18 -0700

    fundamentals

--- basics/Program.cs
+++ basics/Program.cs
@@ -5,17 +5,20 @@ namespace basics
     class Program
     {
         static void Main(string[] args) {
-            //Interpolation
-            // Console.WriteLine($"The answer to 2 + 7 is {2+7}");
-            //Loops
-            // for (int i = 1; i <= 5; i++) {
-            //     Console.WriteLine(i);
-            // } 
-            // int j = 1;
-            // while (j<6){
-            //     Console.WriteLine(j);
-            //     j = j + 1;
-            // }
+            // Interpolation
+            Console.WriteLine($"The answer to 2 + 7 is {2+7}");
+            // Loops
+            for (int i = 1; i <= 5; i++) {
+                Console.WriteLine(i);
+            } 
+            int j = 1;
+            while (j<6){
+                Console.WriteLine(j);
+                j = j + 1;
+            }
+            //Generating Random Numbers
+            Random rand = new Random();
+            Console.WriteLine(rand.Next());
         }
     }
 }
---
+++ fundamentals/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for (int i = 1; i <= 255; i++){
+            //     Console.WriteLine(i);
+            // }
+            // for (int i = 1; i <= 100; i++){
+            //     if (i%3 == 0 && i%5 != 0){
+            //         Console.WriteLine(i);
+            //     }
+            //     else if (i%3 != 0 && i%5 == 0){
+            //         Console.WriteLine(i);
+            //     }
+            // }
+            // for (int i = 1; i <= 100; i++){
+            //     if (i%3 == 0 && i%5 != 0){
+            //         Console.WriteLine($"Fizz {i}");
+            //     }
+            //     else if (i%3 != 0 && i%5 == 0){
+            //         Console.WriteLine($"Buzz {i}");
+            //     }
+            //     else if (i%3 == 0 && i%5 == 0){
+            //         Console.WriteLine($"FizzBuzz {i}");
+            //     }
+            // }
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++){
+                int value = rand.Next();
+                if (value%3 == 0 && value%5 != 0){
+                    Console.WriteLine($"Fizz {value}");
+                }
+                else if (value%3 != 0 && value%5 == 0){
+                    Console.WriteLine($"Buzz {value}");
+                }
+                else if (value%3 == 0 && value%5 == 0){
+                    Console.WriteLine($"FizzBuzz {value}");
+                }
+            }
+        }
+    }
+}
