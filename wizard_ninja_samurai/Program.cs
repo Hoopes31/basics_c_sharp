fe6c7cfeb42da0b30a4106b1a710adf460d6604b
Hoopes31 <anthonyhoopes@gmail.com>
2017-10-17 17:07:47 -0700

    more oop

---
+++ wizard_ninja_samurai/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wizard_ninja_samurai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
