45f04103f8915555691ee834edf4c5998c218612
Anthony <Anthony@Anthonys-iMac.local>
2017-10-31 21:04:16 -0700

    validation date

--- restEntity/Models/Review.cs
+++ restEntity/Models/Review.cs
@@ -5,7 +5,10 @@ namespace restEntity.Models
     {
         public int id { get; set; }
         public string reviewer_name { get; set; }
+        public string review { get; set; }
+        public int stars { get; set; }
         public Restaurant restaurant { get; set; }
+        public DateTime date_visited { get; set; }
         public DateTime created_date { get; set; }
         public DateTime updated_date { get; set; }
     }
