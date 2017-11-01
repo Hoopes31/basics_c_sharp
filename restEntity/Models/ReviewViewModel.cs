dae7920cf2cc523b85638fbeaaed735dc1b2eec6
Anthony <Anthony@Anthonys-iMac.local>
2017-10-31 19:16:59 -0700

    login/reg fix

--- restEntity/Models/ReviewViewModel.cs
+++ restEntity/Models/ReviewViewModel.cs
@@ -12,7 +12,26 @@ namespace restEntity.Models
 
         public Restaurant restaurant { get; set; }
         [Required(ErrorMessage = "Date is required")]
+        [ValidDateAttribute]
         public DateTime date_visited { get; set; }
         public int stars { get; set; }
+    }
+    // Custom Data Annotations for validators
+    public class ValidDateAttribute : ValidationAttribute
+    {
+        private DateTime _today = DateTime.Now;
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            ReviewViewModel restaurant = (ReviewViewModel)validationContext.ObjectInstance;
+            if(_today >= restaurant.date_visited)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Date must be not be in the future");
+            }
+        }
     }
 }
